c8c0ba74901a5a741bf60a07827c8bc5de3e887d
PHPCoder99 <84620860+PHPCoder99@users.noreply.github.com>
2023-06-27 12:04:48 +0700

    4

---
+++ Tree.java
@@ -0,0 +1,66 @@
+class Tree{
+    private Node root;
+    class Node{
+        int value;
+        Node left;
+        Node right;
+
+        Color color;
+    }
+
+    enum Color{
+        BLACK,
+        RED
+    }
+
+    public void insert(int value){
+        if(root == null){
+            root = new Node();
+            root.value = value;
+        }else{
+            insert(root, value);
+            root = balance(root);
+        }
+        root.color = Color.BLACK;
+    }
+
+    private void insert(Node node, int value){
+        if(node.value != value){
+            if(node.value < value){
+                if(node.right == null){
+                    node.right = new Node();
+                    node.right.value = value;
+                   node.right.color = RED;
+                }else{
+                    insert(node.right, value);
+                }
+            }else{
+                if(node.left == null){
+                    node.left = new Node();
+                    node.left.value = value;
+                    node.left.color = RED;
+                }else{
+                    insert(node.left, value);
+                    node.left.color = BLACK;
+                }
+            }
+        }
+    }
+
+    public Node find(int value){
+        return find(root, value);
+    }
+
+    private Node find(Node node, int value) {
+        if(node == null){
+            return null;
+        }
+        if(node.value == value){
+            return node;
+        }
+        if(node.value < value) {
+            return find(node.right, value);
+        }else{
+            return find(node.left, value);
+        }
+    }
